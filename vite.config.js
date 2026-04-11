6010d01cb756473aeac74bf384342078e9f1cce3
srcuman <ydgzhu9936@126.com>
2026-04-11 18:14:50 +0800

    添加Vite代理配置，更新版本号为3.5.8

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://fundgz.1234567.com.cn',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/fund': {
+        target: 'http://fund.eastmoney.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fund/, '')
+      },
+      '/stock': {
+        target: 'http://qt.gtimg.cn',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/stock/, '')
+      }
+    }
+  }
 })
