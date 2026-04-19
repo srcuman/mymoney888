2d4d6d4c34cb42dfa71822f80becfdf3ed2b39df
srcuman <ydgzhu9936@126.com>
2026-04-19 17:42:19 +0800

    项目重命名为发发，版本升级到4.0.0
    更新内容：
    1. 项目名称更新为【发发】
    2. README.md 标题更新
    3. 版本号升级到4.0.0
    4. docker-compose 文件添加注释说明本地构建
    5. 清理和更新文档

--- src/config/version.js
+++ src/config/version.js
@@ -2,12 +2,12 @@
  * 统一版本配置文件
  * 所有涉及版本号的地方都应从此文件导入
  */
-export const APP_VERSION = '3.9.0'
-export const DB_VERSION = '3.9.0'
+export const APP_VERSION = '4.0.0'
+export const DB_VERSION = '4.0.0'
 export const VERSION_INFO = {
-  app: '3.9.0',
-  database: '3.9.0',
+  app: '4.0.0',
+  database: '4.0.0',
   releaseDate: new Date().toISOString().split('T')[0],
-  archVersion: 'v3.9.0 - 数据为核心，标签化存储架构'
+  archVersion: 'v4.0.0 - 项目重命名为发发，架构优化'
 }
 export default APP_VERSION
